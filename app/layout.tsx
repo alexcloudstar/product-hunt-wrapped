7d993a874d2f6d0f11d2398bf3a96bdac0d56cbe
alexcloudstar <alexcloudstar@gmail.com>
2025-12-23 20:58:04 +0200

    feat: Sora Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import type { Metadata } from 'next';
+import { Sora } from 'next/font/google';
+import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const soraFont = Sora({
+  variable: '--font-sora',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['400', '700'],
 });
 
 export const metadata: Metadata = {
@@ -64,9 +60,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${soraFont.variable} ${soraFont.variable} antialiased`}>
         {children}
         <Analytics />
       </body>
